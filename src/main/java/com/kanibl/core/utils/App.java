9fc6fd7127f4482f9cbb3c1218261fcbc4dc652b
Tugdual Grall <tgrall@github.com>
2022-08-02 08:28:06 +0000

    update log4j dependency

--- src/main/java/com/kanibl/core/utils/App.java
+++ src/main/java/com/kanibl/core/utils/App.java
@@ -1,11 +1,12 @@
 package com.kanibl.core.utils;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.joda.time.LocalDate;
 
 public class App {
 
-	private static final Logger logger = Logger.getLogger(App.class);
+    private static Logger logger = LogManager.getLogger(App.class);
 
 	public static void main(String[] args) {
 		System.out.println(getLocalCurrentDate());
